63ebb8c16f6dd8a43275930f32c346bb7568ae7f
지은 <vividdream0215@naver.com>
2021-11-08 17:36:39 +0900

    1번문제

--- Algorithm_java/src/_1/Main.java
+++ Algorithm_java/src/_1/Main.java
@@ -1,11 +1,27 @@
 package _1;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Student st1 = new Student("손오공", "1682");
+		Student st2 = new Student("저팔계", "1111");
+		Student st3 = new Student("사오정", "2222");
+		
+		ArrayList<Student> list = new ArrayList<Student>();
 
+		list.add(st1);
+		list.add(st2);
+		list.add(st3);
+		
+		for(Student stu : list) {
+			System.out.println(stu.getName());
+			System.out.println(stu.getNo());
+			
+		}
+		
 	}
 
 }
