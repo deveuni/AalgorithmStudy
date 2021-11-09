487b7f2115e528d2136294a0c486e9e81a57c822
지은 <vividdream0215@naver.com>
2021-11-09 20:37:43 +0900

    1번 해설

--- Algorithm_java/src/_1/Main.java
+++ Algorithm_java/src/_1/Main.java
@@ -1,6 +1,7 @@
 package _1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -19,8 +20,25 @@ public class Main {
 		for(Student stu : list) {
 			System.out.println(stu.getName());
 			System.out.println(stu.getNo());
+		}
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("계속 검색을 하고 싶으면 y, 종료하고 싶으면 n");
+		
+		
+		while(true) {
+			String input = scan.next();
+			
+			if(input.equals("y")) {
+				
+			}else if(input.equals("n")) {
+				break;
+			}
 			
 		}
+		
+		System.out.println("프로그램이 종료되었습니다.");
 		
 	}
 
