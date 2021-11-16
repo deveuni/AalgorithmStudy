3272e472049bc38e11868ace14f9084a1b0425a8
지은 <vividdream0215@naver.com>
2021-11-16 18:18:35 +0900

    3번 해설

--- Algorithm_java/src/_3/Main.java
+++ Algorithm_java/src/_3/Main.java
@@ -31,6 +31,35 @@ public class Main {
 		// 2: 4 --> 최변수
 		// 3: 2
 		// 4: 2
+		
+		int []mode = new int[10];
+		
+		//index -> 출현한 수 
+		// index 값 -> index(출현한 수)가 몇번 나왔는지 저장하는 용도 
+		
+		// "mode[3] = 5 => 3번 숫자가, 5번 출현했다"
+		
+		// inputNum => 1 2 2 3 1 4 2 2 4 3 
+		for(int i=0; i<10; i++) {
+			mode[inputNum[i]]++;
+		}
+		
+		// mode[1] == 2
+		// mode[2] == 4
+		// mode[3] == 2
+		// mode[4] == 2
+		
+		int modeNum = 0; // 최변수 
+		int modeCnt = 0; // 최변수가 나온 횟수 
+		
+		for(int i=0; i<10; i++) {
+			if(modeCnt < mode[i]) {
+				modeCnt = mode[i];
+				modeNum = i;
+			}
+		}
+		
+		System.out.println("최변수는:"+modeNum+" cnt:"+modeCnt);
 	}
 
 }
