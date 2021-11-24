4ed38b2a7da91d2ab42228989b2adc78f90eaf4b
지은 <vividdream0215@naver.com>
2021-11-24 20:43:55 +0900

    2557 Hello World

---
+++ Algorithm_java/src/baekjoon/_2557.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+
+// https://www.acmicpc.net/problem/2557
+// Hello World
+public class _2557 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
