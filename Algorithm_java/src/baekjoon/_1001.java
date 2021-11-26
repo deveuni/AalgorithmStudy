bb6cde2b6ed54bcad3d0c9eb90d35226b6cc796b
지은 <vividdream0215@naver.com>
2021-11-27 00:35:44 +0900

    1001 A-B

---
+++ Algorithm_java/src/baekjoon/_1001.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/1001
+// A-B
+
+public class _1001 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		System.out.println(A-B);
+		
+		
+		
+		
+	}
+
+}
