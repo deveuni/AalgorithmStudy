d0028f887032f7706788ff1ded77ed862afe10a8
지은 <vividdream0215@naver.com>
2021-11-26 15:15:29 +0900

    10172 개

---
+++ Algorithm_java/src/baekjoon/_10172.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+// https://www.acmicpc.net/problem/10172
+// 개
+
+public class _10172 {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|\n");
+		sb.append("|q p|   /}\n");
+		sb.append("( 0 )\"\"\"\\\n");
+		sb.append("|\"^\"`    |\n");
+		sb.append("||_/=\\\\__|\n");
+		
+		System.out.println(sb);
+		
+	}
+
+}
