dd2d486b5e2df5a8d7b4ffa6e10a6fdc88308c20
지은 <vividdream0215@naver.com>
2021-11-30 15:14:40 +0900

    10869 사칙연산

---
+++ Algorithm_java/src/baekjoon/_10869.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/10869
+// 사칙연산
+
+public class _10869 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		StringTokenizer st = new StringTokenizer(str, " ");
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+				
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+		
+	}
+
+}
