a2e2cc8d10ff639396d1b05fb9a04fea64dce1b0
지은 <vividdream0215@naver.com>
2021-12-03 19:42:26 +0900

    2588 곱셈

--- Algorithm_java/src/baekjoon/_2588.java
+++ Algorithm_java/src/baekjoon/_2588.java
@@ -14,17 +14,34 @@ public class _2588 {
 	public static void main(String[] args) {
 		
 		// 방법 1
-		Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 		
 		int A = sc.nextInt();
 		String B = sc.next();
 		
 		sc.close();
 		
+		
 		System.out.println(A *(B.charAt(2) - '0'));
 		System.out.println(A *(B.charAt(1) - '0'));
 		System.out.println(A *(B.charAt(0) - '0'));
 		System.out.println(A * Integer.parseInt(B));
+		*/
+		
+		// 방법 2 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A*(B%10));
+		System.out.println(A*(B%100/10));
+		System.out.println(A*(B/100));
+		System.out.println(A*B);
+		
+		
+		
 	}
 
 }
