92eff342643476e5e7d4d485bfb2525e882d063c
지은 <vividdream0215@naver.com>
2021-12-04 00:03:11 +0900

    2588 곱셈

--- Algorithm_java/src/baekjoon/_2588.java
+++ Algorithm_java/src/baekjoon/_2588.java
@@ -1,5 +1,8 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/2588
@@ -11,7 +14,7 @@ import java.util.Scanner;
 // 참고 출처 : https://st-lab.tistory.com/20
 public class _2588 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		// 방법 1
 		/*Scanner sc = new Scanner(System.in);
@@ -28,9 +31,16 @@ public class _2588 {
 		System.out.println(A * Integer.parseInt(B));
 		*/
 		
+		// charAt() 
+		// 문자열의 인덱스 위치에 있는 문자(Character) 값을 반환한다.
+		// 반환되는 값은 아스키코드값인 문자(char)이다.
+		// 따라서 각각의 문자열 인덱스를 참조한 뒤 -'0'을 해주는 이유 또한 
+		// 우리가 문자로 저장된 숫자가 아닌 우리가 보는 숫자 그대로의 값을 쓰기 위한 것
+		
+		
 		// 방법 2 
 		
-		Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 		
 		int A = sc.nextInt();
 		int B = sc.nextInt();
@@ -39,6 +49,26 @@ public class _2588 {
 		System.out.println(A*(B%100/10));
 		System.out.println(A*(B/100));
 		System.out.println(A*B);
+		
+		*/
+		
+		// 방법 3
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		int A = Integer.parseInt(br.readLine());
+		String B = br.readLine();
+		
+		char[] b = B.toCharArray();
+		// toCharArray() 
+		// => 문자열을  char 배열 형태로 반환해주는 메소드 
+		// => 즉, character 배열로 만들어 준 뒤에 하나씩 배열에서 꺼내서 쓰는 방법.
+		// => 문자열 길이가 가변적으로 입력할 때, 문자 하나씩 참조해야할 때 매우 유용하다.
+		
+		
+		System.out.println(A * (b[2]='0'));
+		System.out.println(A * (b[1]='0'));
+		System.out.println(A * (b[0]='0'));
+		System.out.println(A * Integer.parseInt(B));
 		
 		
 		
