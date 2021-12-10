9586e049d0d2a167766e4738e82737f32bb3c727
지은 <vividdream0215@naver.com>
2021-12-10 17:38:31 +0900

    2884 알람 시계

---
+++ Algorithm_java/src/baekjoon/_2884.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2884
+// 알람 시계
+
+public class _2884 {
+
+	public static void main(String[] args) throws IOException {
+		
+		/*
+		 * 1-1 단계 : 45분보다 작은지 여부 - if(min < 45)
+		 * 	2-1 단계 : 시(hour)가 0보다 작은지 여부 - if(hour < 0)
+		 * 1-2 단계 : 1단계 조건의 외 여부 - else
+		*/
+		
+		// 참고로 분 60분이니 만약 입력값이 45보다 작을 경우 (45-입력값)의 차이만큼 60에서 빼주면 된다. 
+		// 즉, 60-(45-입력값)이 된다.
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int H = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+			if(M < 45 ) {
+			H--;			// 시(hour) 1 감소
+			M = 60- (45 - M); // 분(min) 감소
+			if(H < 0) {
+				H = 23;
+			}
+			System.out.println(H + " " + M);
+		} else {
+			System.out.println(H + " " + (M - 45));
+		}
+		
+	}
+	
+	
+	
+	
+
+}
