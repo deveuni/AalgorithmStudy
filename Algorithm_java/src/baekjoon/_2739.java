d67f160c161eb27353cd653844837ac43b89435a
지은 <vividdream0215@naver.com>
2021-12-12 19:28:01 +0900

    2739 구구단

--- Algorithm_java/src/baekjoon/_2739.java
+++ Algorithm_java/src/baekjoon/_2739.java
@@ -1,5 +1,10 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/2739
@@ -7,8 +12,9 @@ import java.util.Scanner;
 
 public class _2739 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		// 방법 1 Scanner
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
@@ -18,6 +24,40 @@ public class _2739 {
 		for(int i=1; i<=9; i++) {
 			System.out.println(N + " * " + i + " = "  + N*i);
 		}
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		br.close();
+		
+		// 방법 2 
+		/*
+		for(int i=1; i<10;i++) {
+			System.out.println(a+" * "+i+" = "+(a*i));
+		}
+		*/
+		
+		// 방법 3
+		/*
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i =1; i<10; i++) {
+			sb.append(a).append(' ').append('*').append(' ').append(i);
+			sb.append(' ').append('=').append(' ').append(a*i).append('\n');
+		}
+		System.out.println(sb);
+		*/
+		
+		// 방법 4
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i=1; i<10;i++) {
+			bw.write(a+" * "+i+" = "+a*i+"\n");
+		}
+		bw.flush();
+		bw.close();
+		
 		
 	}
 
