9e391255e76143e33611664433a67b5928f1d60b
지은 <vividdream0215@naver.com>
2021-12-12 18:35:11 +0900

    2739 구구단

---
+++ Algorithm_java/src/baekjoon/_2739.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/2739
+// 구구단
+
+public class _2739 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(N + " * " + i + " = "  + N*i);
+		}
+		
+	}
+
+}
