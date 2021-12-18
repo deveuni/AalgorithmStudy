58a4c5cf9055200842bb956160c02e62d01b17ec
지은 <vividdream0215@naver.com>
2021-12-18 17:44:47 +0900

    11021 A+B-7

--- Algorithm_java/src/baekjoon/_11021.java
+++ Algorithm_java/src/baekjoon/_11021.java
@@ -1,26 +1,48 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/11021
 // A+B-7
 
 public class _11021 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		// 방법 1
+		/*
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
+		int T = sc.nextInt();
 		
-		for (int i =1; i <=a;i++) {
-			int c = sc.nextInt();
-			int d = sc.nextInt();
+		for (int i = 1; i <= T; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
 			
-			System.out.println("Case #" + i + ": " + (c + d));
+			System.out.println("Case #" + i + ": " + (a + b));
 			
 			sc.close();
 		}
+		*/
+		
+		// 방법 2 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		for(int i = 1; i <= T; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			System.out.println("Case #" + i + ": "
+					+(Integer.parseInt(st.nextToken())
+					+Integer.parseInt(st.nextToken())));
+		}
+		br.close();
+		
 	}
 
 }
