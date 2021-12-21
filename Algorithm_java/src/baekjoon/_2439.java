d26c656bba700efade26f7b25d6150cb5d33cb60
지은 <vividdream0215@naver.com>
2021-12-21 21:07:01 +0900

    2439 별찍기-2

---
+++ Algorithm_java/src/baekjoon/_2439.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/2439
+// 별 찍기 - 2
+
+public class _2439 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		// 공백이 1행에 4개, 2행헤 3개 출력,
+		// 즉, n번째 행에는 N-n개의 공백이 출력되고, 별은 n번째 행에 n개 출력되어야 한다.
+		
+		for (int i=1; i <= N; i++) {
+			for(int j=1; j<=N-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for (int k=1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
