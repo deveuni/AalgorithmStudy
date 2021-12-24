55b073468eeaac321b3ec066e2c02c0db0e5f8d6
지은 <vividdream0215@naver.com>
2021-12-24 19:37:59 +0900

    10871 X보다 작은 수

---
+++ Algorithm_java/src/baekjoon/_10871.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/10871
+// X보다 작은 수
+
+public class _10871 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int arr[] = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		for(int i =0; i < N; i++) {
+			if(arr[i] < X) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+
+}
