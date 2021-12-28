0e54c5babb38647eeadc8c1987dc08504b1d9467
지은 <vividdream0215@naver.com>
2021-12-28 18:36:54 +0900

    10951 A+B-4

--- Algorithm_java/src/baekjoon/_10951.java
+++ Algorithm_java/src/baekjoon/_10951.java
@@ -1,12 +1,14 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/10951
 // A+B-4
+//입력의 종료는 더이상 읽을 수 있는 데잍 (EOF)가 없을 때 종료한다.
 
 public class _10951 {
 
@@ -17,6 +19,7 @@ public class _10951 {
 		StringTokenizer st;
 		String str;
 		
+		// 방법 1 
 		while( (str = br.readLine()) != null) {
 			
 			st = new StringTokenizer(str," ");
@@ -28,6 +31,16 @@ public class _10951 {
 		}
 		
 		System.out.print(sb);
+		
+		
+		// 방법2 
+		while( (str=br.readLine()) != null) {
+			
+			int a = str.charAt(0) - 48;
+			int b = str.charAt(2) - 48;
+			sb.append(a+b).append("\n");
+		}
+		System.out.println(sb);
 	}
 
 }
