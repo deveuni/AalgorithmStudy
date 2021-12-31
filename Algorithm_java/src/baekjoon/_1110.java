fce42969b373290f89cede2a50050a561d97f084
지은 <vividdream0215@naver.com>
2021-12-31 09:36:15 +0900

    1110 더하기 사이클

--- Algorithm_java/src/baekjoon/_1110.java
+++ Algorithm_java/src/baekjoon/_1110.java
@@ -1,5 +1,8 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/1110
@@ -7,7 +10,9 @@ import java.util.Scanner;
 
 public class _1110 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		/*
 		Scanner in = new Scanner(System.in);
 		 
 		int N = in.nextInt();
@@ -25,7 +30,23 @@ public class _1110 {
 			}
 		}
 		System.out.println(cnt);
-	
+	*/
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		int copy = N;
+        
+		do {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			cnt++;
+		} while (copy != N);
+		
+		System.out.println(cnt);
+		
+		
 		
 	}
 
