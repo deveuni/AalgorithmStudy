0aed455d3358ad6d8799609f5cffb72cf6ca35d2
지은 <vividdream0215@naver.com>
2022-01-02 12:50:45 +0900

    10818 최소, 최대

--- Algorithm_java/src/baekjoon/_10818.java
+++ Algorithm_java/src/baekjoon/_10818.java
@@ -1,15 +1,20 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/10818
 // 최소,최대 
 
 public class _10818 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		/*
 		Scanner in = new Scanner(System.in);
         
 		int N = in.nextInt();
@@ -22,6 +27,23 @@ public class _10818 {
 		in.close();
 		Arrays.sort(arr);
 		System.out.print(arr[0] + " " + arr[N - 1]);
+		*/
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int index = 0;
+		int[] arr = new int[N];
+		while(st.hasMoreTokens()) {
+			arr[index] = Integer.parseInt(st.nextToken());
+			index++;
+		}
+		
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[N - 1]);
+		
 
 	}
 
