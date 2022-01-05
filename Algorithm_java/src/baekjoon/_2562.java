8be7a6e333f722ce0d603d2e0a79b85bfa4d3a1e
지은 <vividdream0215@naver.com>
2022-01-06 03:04:18 +0900

    2562 최댓값

--- Algorithm_java/src/baekjoon/_2562.java
+++ Algorithm_java/src/baekjoon/_2562.java
@@ -1,5 +1,8 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/2562
@@ -7,8 +10,11 @@ import java.util.Scanner;
 
 public class _2562 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		
+		// 방법 1
+		/*
 		Scanner in = new Scanner(System.in);
 		int[] arr = { in.nextInt(), in.nextInt(), in.nextInt(),
 				in.nextInt(), in.nextInt(), in.nextInt(),
@@ -28,6 +34,35 @@ public class _2562 {
 			}
 		}
 		System.out.print(max + "\n" + index);
+		*/
+		
+		// 방법 2 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[9];
+		
+		
+		for(int i=0; i < 9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int max = 0;
+		int index = 0;
+		
+		int count = 0;
+		
+		for(int value : arr) {
+			count++;
+			if(value > max) {
+				max = value;
+				index = count;
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(index);
+		
+		
 	}
 
 }
