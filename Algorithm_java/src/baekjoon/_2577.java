6681dd60b11e0d1c0ff3fa6ed5acae0da7b6d24b
지은 <vividdream0215@naver.com>
2022-01-08 03:18:46 +0900

    2577 숫자의 개수

--- Algorithm_java/src/baekjoon/_2577.java
+++ Algorithm_java/src/baekjoon/_2577.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 // https://www.acmicpc.net/problem/2577
 // 숫자의 개수
 
+// Integer.toString()
+// => int형을 String으로 타입을 변환시킨다.
+
 public class _2577 {
 
 	public static void main(String[] args) {
 		
 
+		// 방법 1 
+		/*
 		Scanner in = new Scanner(System.in);
  
 		int value = (in.nextInt() * in.nextInt() * in.nextInt());
@@ -25,7 +30,17 @@ public class _2577 {
 			}
 			System.out.println(count);
 		}
+		
+		// 첫번째 for문은 0~9까지 각가의 수를 검사할 반복문이다.
+		// 그리고 count로 개수를 카운트 하기 위한 변수를 0으로 초기화 한 후, 
+		// 이중 for문을 통해 str의 길이만큼 모든 요소를 검사한다. 
+		// charAt()을 통해  해당 인덱스의 문자와 i가 같은 수가 몇개 있는지
+		// 보며 만약 같을 경우 count를 +1 증가시킨다.
 
+		*/
+		
+		// 방법 2 
+		
 	}
 
 }
