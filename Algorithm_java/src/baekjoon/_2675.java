0736da950ea10aa9f2945b889c53c5d44b4da7a7
지은 <vividdream0215@naver.com>
2022-01-28 16:17:53 +0900

    2675 문자열 반복

--- Algorithm_java/src/baekjoon/_2675.java
+++ Algorithm_java/src/baekjoon/_2675.java
@@ -1,5 +1,8 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/2675
@@ -10,10 +13,10 @@ import java.util.Scanner;
 
 public class _2675 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		// 방법 1 - Scanner
-		
+		/*
 		Scanner in = new Scanner(System.in);
 		
 		int T = in.nextInt(); // 테스트 케이스의 개수 
@@ -32,6 +35,32 @@ public class _2675 {
 			}
 			
 			System.out.println();
+			*/
+			
+			
+			// 방법 2 - BufferedReader 
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			int T = Integer.parseInt(br.readLine());
+			
+			for(int i=0; i < T; i++) {
+				
+				String[] str = br.readLine().split(" "); // 공백 분리
+				
+				int R = Integer.parseInt(str[0]); // String -> int
+				String S = str[1];
+				
+				for(int j=0; j < S.length(); j++) {
+					for(int k=0; k<R; k++) {
+						System.out.println(S.charAt(j));
+					}
+				}
+
+				System.out.println();
+			
+			
+			
 		}
 
 	}
