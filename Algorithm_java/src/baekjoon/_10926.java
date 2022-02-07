482f27b234a5120271b0e00ee29e82a3a31f6490
지은 <vividdream0215@naver.com>
2022-02-07 15:17:05 +0900

    10926 ??!

---
+++ Algorithm_java/src/baekjoon/_10926.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/10926
+// ??
+
+public class _10926 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next() + "??!");
+	}
+
+}
