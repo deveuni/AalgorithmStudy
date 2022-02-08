34e1a26687583451372b9d5cc7bc8fff29b38e89
지은 <vividdream0215@naver.com>
2022-02-08 12:08:41 +0900

    18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ Algorithm_java/src/baekjoon/_18108.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/18108
+// 1998년생인 내가 태국에서는 2541년생?!
+
+public class _18108 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		System.out.println(n-543);
+		
+	}
+
+}
