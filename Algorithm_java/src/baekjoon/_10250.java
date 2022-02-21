e23cf9c79011f311314f83d7768bd8ab78821476
지은 <vividdream0215@naver.com>
2022-02-22 07:43:49 +0900

    10250

--- Algorithm_java/src/baekjoon/_10250.java
+++ Algorithm_java/src/baekjoon/_10250.java
@@ -1,12 +1,16 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class _10250 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		Scanner in = new Scanner(System.in);
+		/*Scanner in = new Scanner(System.in);
 		 
 		int T = in.nextInt();	// 테스트 케이스
  
@@ -23,7 +27,33 @@ public class _10250 {
 			} else {
 				System.out.println(((N % H) * 100) + ((N / H) + 1));
 			}
+			*/
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());	// 테스트 케이스
+ 
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+			int H = Integer.parseInt(st.nextToken());
+			st.nextToken();			// W 는 그냥 버린다.
+			int N = Integer.parseInt(st.nextToken());
+ 
+			if (N % H == 0) {
+				sb.append((H * 100) + (N / H)).append('\n');
+ 
+			} else {
+				sb.append(((N % H) * 100) + ((N / H) + 1)).append('\n');
+			}
+		}
+		System.out.print(sb);
+		
+		
+		
+		
 		}
 	}
 
-}
+
